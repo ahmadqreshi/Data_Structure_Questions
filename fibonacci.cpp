e2b21dccbe5ee394d57ef5dd9252d9840481ece2
Ahmad Qureshi <81093987+ahmadqreshi@users.noreply.github.com>
2022-07-27 18:36:50 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -16,3 +16,12 @@ int main()
     cout<<fib(6);
     return 0;
 }
+
+//method 2 - by using direct formula O(log n ) because of pow function has logn time complexity
+class Solution {
+public:
+    int fib(int n) {
+        double phi = (sqrt(5) + 1)/2;
+        return round(pow(phi,n)/sqrt(5));
+    }
+};
