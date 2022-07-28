d455f30cc948bb29f79e04f69bb79a3f109d8301
Ahmad Qureshi <81093987+ahmadqreshi@users.noreply.github.com>
2022-07-28 12:59:04 +0530

    Update binary_tree_level_order.cpp

--- binary_tree_level_order.cpp
+++ binary_tree_level_order.cpp
@@ -13,15 +13,37 @@ class Node {
         right = NULL;
     }
 };
-Node* buildTree() { 
+Node* preOrderBuildTree() { 
     int d;
     cin>>d;
     if(d==-1)
         return NULL;
     Node* n = new Node(d);
-    n->left = buildTree();
-    n->right = buildTree();
+    n->left = preOrderBuildTree();
+    n->right = preOrderBuildTree();
     return n;
+}
+Node* levelOrderBuild() { // take input without using recursion // use queue to store root first and then left and right child of the front
+    int val;
+    cin>>val;
+    Node* root = new Node(val);
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()) {
+        Node* current = q.front();
+        q.pop();
+        int val1,val2;
+        cin>>val1>>val2;
+        if(val1!=-1) {
+            current->left = new Node(val1);
+            q.push(current->left);
+        }
+        if(val2!=-1) {
+            current->right = new Node(val2);
+            q.push(current->right);
+        }
+    }
+    return root;
 }
 void printLevelOrder(Node* root) { //use queue data structure 
     queue<Node*> q;
@@ -47,18 +69,25 @@ void printLevelOrder(Node* root) { //use queue data structure
 }
 int main()
 {
-    //input //1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
-    // int n = sizeof(arr)/sizeof(int);
-    // while(n>=0) {
-    //     buildTree();
-    //     n--;
-    // }
-    Node* root = buildTree();
+  
+    cout<<"Give PreOrder input"<<endl;
+    Node* root = preOrderBuildTree(); //input is in preorder //1 2 4 -1 -1 5 7 -1 -1 -1 3 -1 6 -1 -1
+    cout<<"Give level Order Input"<<endl;
+    Node* root1 = levelOrderBuild(); // input is in level order // 1 2 3 4 5 -1 6 -1 -1 -1 -1 7 -1 -1 -1
+    cout<<"print Level order of both trees"<<endl;
     printLevelOrder(root);
+    cout<<endl;
+    printLevelOrder(root1);
     return 0;
 }
 //OUTPUT
 /*
+print Level order of both trees
+1
+23
+456
+7
+
 1
 23
 456
